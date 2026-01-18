e58ae1de360adaeb0101bfa9bf36a609276f53be
Reda-Kriate <kriatereda@gmail.com>
2026-01-18 19:29:04 +0100

    fixing controller with given context

--- src/main/java/org/reda/rag_simplevs/RagControler.java
+++ src/main/java/org/reda/rag_simplevs/RagControler.java
@@ -2,35 +2,45 @@ package org.reda.rag_simplevs;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.document.ContentFormatter;
+import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.scheduler.Schedulers;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
 @RestController
 public class RagControler {
     private ChatClient chatClient;
-//    @Autowired
-//    private VectorStore vectorStore;
 
-    public RagControler(ChatClient.Builder builder ) {
+    private VectorStore vectorStore;
+
+    @Value("classpath:/prompt/prompt.st")
+    private Resource promptResource;
+
+    public RagControler(ChatClient.Builder builder, VectorStore vectorStore) {
+        this.vectorStore = vectorStore;
         this.chatClient = builder.build();
-//                .defaultAdvisors(
-//                         QuestionAnswerAdvisor.builder(vectorStore)
-//                                 .searchRequest(SearchRequest.builder().build())
-//                                 .promptTemplate(new PromptTemplate(new ClassPathResource("prompt/prompt.st")))
-//                                 .scheduler(Schedulers.boundedElastic())
-//                                .build()
-//                )
     }
 
     @GetMapping("/ask")
     public String ask(String question){
-        return chatClient.prompt().user(question).call().content();
+        List<Document> documents = vectorStore.similaritySearch(question);
+        List<ContentFormatter> context = documents.stream().map(Document::getContentFormatter).toList();
+        PromptTemplate promptTemplate = new PromptTemplate(promptResource);
+        Prompt prompt = promptTemplate.create(Map.of("context", context, "question", question));
+        return chatClient.prompt(prompt).call().content();
     }
 }
